cef2239334bdbd65ae6fad5e6e499452b3a47e0b
Sagivm <Sgmelamed@gmail.com>
2023-04-23 16:08:53 +0300

    fix optimizer and update network

--- Ex2/main.py
+++ Ex2/main.py
@@ -1,22 +1,20 @@
+import tensorflow as tf
 from Ex2.util.format_dataset import format_dataset
 from Ex2 import TRAIN_PATH, TEST_PATH , BATCH_SIZE, EPOCHS
 from Ex2.siamese_model import build_base_model, build_siamese_model, batch_generator
-from keras.optimizers import SGD
+from keras.optimizers import SGD,Adam
 import numpy as np
 import imageio
 
 def get_dataset():
-    train = format_dataset(TRAIN_PATH)[:1000]
-    test = format_dataset(TEST_PATH)[:200]
+    train = format_dataset(TRAIN_PATH)[::5]
+    test = format_dataset(TEST_PATH)[::5]
     model = build_siamese_model((250, 250, 1))
-    # train = [([np.asarray(imageio.imread(train[i][0][0])), np.asarray(imageio.imread(train[i][0][1]))],train[i][1]) for i in range (1,10)]
-    # test = [([np.asarray(imageio.imread(test[i][0][0])), np.asarray(imageio.imread(test[i][0][1]))], test[i][1]) for i in range(1, 10)]
     train_batch_generator = batch_generator(train, BATCH_SIZE)
     val_batch_generator = batch_generator(test, BATCH_SIZE)
 
-    # test_batch_generator = batch_generator(test, BATCH_SIZE)
-
-    opt = SGD(lr=0.01, momentum=0.9)
+    opt = SGD(lr=0.001, momentum=0.5)
+    # opt = Adam(learning_rate=0.01)
     model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
     print(model.summary())
@@ -26,7 +24,7 @@ def get_dataset():
             steps_per_epoch=len(train) // BATCH_SIZE,
             epochs=EPOCHS,
             validation_data=val_batch_generator,
-            validation_steps=len(test)// BATCH_SIZE)
+            validation_steps=len(test) // BATCH_SIZE)
 
 
 get_dataset()
--- Ex2/siamese_model.py
+++ Ex2/siamese_model.py
@@ -9,20 +9,34 @@ from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import GlobalAveragePooling2D
 from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import Flatten
-
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.regularizers import l2
+# from s.transform import rotate, AffineTransform, warp, rescale
+import random
 
 def build_base_model(input_shape):
+    W_init_1 = RandomNormal(mean=0, stddev=0.01)
+    b_init = RandomNormal(mean=0.5, stddev=0.01)
+    W_init_2 = RandomNormal(mean=0, stddev=0.2)
+
     model = tf.keras.Sequential([
         Input(input_shape),
         Conv2D(64, (10, 10), padding="valid", activation="relu"),
         MaxPooling2D((2, 2)),
-        Conv2D(128, (7, 7), padding="valid", activation="relu"),
+        Conv2D(128, (7, 7), padding="valid", activation="relu", kernel_initializer=W_init_1, bias_initializer=b_init,
+               kernel_regularizer=l2(2e-4)),
         MaxPooling2D((2, 2)),
-        Conv2D(128, (4, 4), padding="valid", activation="relu"),
+        Conv2D(128, (4, 4), padding="valid", activation="relu", kernel_initializer=W_init_1, bias_initializer=b_init,
+               kernel_regularizer=l2(2e-4)),
+        Conv2D(128, (4, 4), padding="valid", activation="relu", kernel_initializer=W_init_1, bias_initializer=b_init,
+               kernel_regularizer=l2(2e-4)),
         MaxPooling2D((2, 2)),
-        Conv2D(256, (4, 4), padding="valid", activation="relu"),
+        Conv2D(256, (4, 4), padding="valid", activation="relu", kernel_initializer=W_init_1, bias_initializer=b_init,
+               kernel_regularizer=l2(2e-4)),
+        Conv2D(256, (4, 4), padding="valid", activation="relu", kernel_initializer=W_init_1, bias_initializer=b_init,
+               kernel_regularizer=l2(2e-4)),
         Flatten(),
-        Dense(1048, activation="sigmoid")
+        Dense(2048, activation="sigmoid")
     ])
     return model
 
@@ -54,3 +68,29 @@ def batch_generator(pairs, batch_size):
             batch_b = [np.asarray(imageio.imread(pair[0][1])) for pair in batch_pairs]
             batch_labels = [pair[1] for pair in batch_pairs]
             yield [tf.stack(batch_a), tf.stack(batch_b)], tf.stack(batch_labels)
+
+
+
+# def affinetransform(image):
+#     transform = AffineTransform(translation=(-30,0))
+#     warp_image = warp(image,transform, mode="wrap")
+#     return warp_image
+#
+# def anticlockwise_rotation(image):
+#     angle= random.randint(0,45)
+#     return rotate(image, angle)
+#
+# def clockwise_rotation(image):
+#     angle= random.randint(0,45)
+#     return rotate(image, -angle)
+#
+#
+# def transform(image):
+#     if random.random() > 0.5:
+#         image = affinetransform(image)
+#     if random.random() > 0.5:
+#         image = anticlockwise_rotation(image)
+#     if random.random() > 0.5:
+#         image = clockwise_rotation(image)
+#
+#     return image
